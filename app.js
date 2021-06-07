1782deed7e0448036f172b495d367d59f998166d
Diego González <diegoztrombone@gmail.com>
2021-06-07 23:26:57 +0200

    feat: rama nueva, creada navegación, imprime 25 cervezas por pagina, todo  en una página

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const beerSectionNode = document.getElementById("beerSection")
-const myBntNode = document.getElementById("myBnt")
+const randomBeer = document.getElementById("myBnt")
 const myPlusBnt = document.getElementById("pagePlus")
 const myLessBnt = document.getElementById("pageLess")
 const apiUrl = "https://api.punkapi.com/v2/beers"
 const randomApiUrl = "https://api.punkapi.com/v2/beers/random"
 
+myLessBnt.style.visibility = "hidden"
 
-let counter = 1
+
+const counterObj = {
+    counter : 1,
+    upper: function () {
+        this.counter++
+        if (this.counter >= 1) {myLessBnt.style.visibility = "visible"}
+        if (this.counter >= 13) {myPlusBnt.style.visibility = "hidden"}  
+        return this.counter
+    },
+    downer: function () {
+        this.counter--
+        if (this.counter <= 1) {myLessBnt.style.visibility = "hidden"}
+        if (this.counter <= 12){myPlusBnt.style.visibility = "visible"} 
+        return this.counter
+    } 
+}
+const dataCb = x => beerSectionNode.innerHTML += printBeer(x)
+const printBeer = data => `<h3>${data.name}</h3><p>${data.description}</p>`
 
 const fetchedData = (url) => {
     return fetch(url)
@@ -20,34 +38,30 @@ fetchedData(apiUrl)
         
 })
 
-myBntNode.addEventListener("click", () => {
+randomBeer.addEventListener("click", () => {
     beerSectionNode.innerHTML = ""
     fetchedData(randomApiUrl)
         .then(data => data.map(dataCb))
 })
 
 myPlusBnt.addEventListener("click", () => {
-    counter++
+    counterObj.upper()
     beerSectionNode.innerHTML = ""
-    fetchedData(`https://api.punkapi.com/v2/beers?page=${counter}&per_page=25`)
+    fetchedData(`https://api.punkapi.com/v2/beers?page=${counterObj.counter}&per_page=25`)
         .then(data => data.map(dataCb))
+    
     
 })
 
 myLessBnt.addEventListener("click", () => {
-    counter--
+    counterObj.downer()
     beerSectionNode.innerHTML = ""
-    fetchedData(`https://api.punkapi.com/v2/beers?page=${counter}&per_page=25`)
-        .then(data => data.map(dataCb))
-    console.log(counter)
+    fetchedData(`https://api.punkapi.com/v2/beers?page=${counterObj.counter}&per_page=25`)
+        .then(data => data.map(dataCb)) 
+      
 })
 
 
-
-const dataCb = x => beerSectionNode.innerHTML += printBeer(x)
-
-
-const printBeer = data => `<h3>${data.name}</h3><p>${data.description}</p>`
 
 
 
