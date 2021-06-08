62cc56adc39af7a0ffda9e3f6435a220475a6481
Diego González <diegoztrombone@gmail.com>
2021-06-08 21:14:54 +0200

    feat: cambios menores

--- random/random.js
+++ random/random.js
@@ -8,7 +8,7 @@ const randomApiUrl = "https://api.punkapi.com/v2/beers/random"
 
 
 const dataCb = x => beerSectionNode.innerHTML += printBeer(x)
-const printBeer = data => `<h3>${data.name}</h3><p>${data.description}</p>`
+const printBeer = data => `<div class="tarjetClass"><div class="imgContainer"><img src="${data.image_url}"></div><div class="descriptionBox"><h3>${data.name}</h3><p>${data.description}</p></div></div>`
 
 const fetchedData = (url) => {
     return fetch(url)
