834e65f77bd44951731b9c7b16667a5a139ab5f2
Diego González <diegoztrombone@gmail.com>
2021-06-09 21:32:06 +0200

    feat: creado buscador, pagina de descripciones, JS acabado salvo el mapa

--- apps/app.js
+++ apps/app.js
@@ -3,61 +3,111 @@
 // Work in CSS, mobile first
 // Search engine
 
-const beerSectionNode = document.getElementById("beerSection")
-const randomBeer = document.getElementById("myBnt")
-const myPlusBnt = document.getElementById("pagePlus")
-const myLessBnt = document.getElementById("pageLess")
-const apiUrl = "https://api.punkapi.com/v2/beers"
-const randomApiUrl = "https://api.punkapi.com/v2/beers/random"
-
-
+const beerSectionNode = document.getElementById("beerSection");
+const randomBeer = document.getElementById("randomBtn");
+const myPlusBnt = document.getElementById("pagePlus");
+const myLessBnt = document.getElementById("pageLess");
+const apiUrl = "https://api.punkapi.com/v2/beers?page=1&per_page=10";
+const randomApiUrl = "https://api.punkapi.com/v2/beers/random";
+const searchNode = document.getElementById("searchBar");
+const buttonNode = document.getElementById("searchButton");
+const allBeerArr = [
+  "https://api.punkapi.com/v2/beers?page=1&per_page=80",
+  "https://api.punkapi.com/v2/beers?page=2&per_page=80",
+  "https://api.punkapi.com/v2/beers?page=3&per_page=80",
+  "https://api.punkapi.com/v2/beers?page=4&per_page=80",
+  "https://api.punkapi.com/v2/beers?page=5&per_page=80",
+];
 const counterObj = {
-    counter : 1,
-}
+  counter: 1,
+};
 
-const dataCb = x => beerSectionNode.innerHTML += printBeer(x)
-const printBeer = data => `<div class="tarjetClass"><div class="imgContainer"><img src="${data.image_url}"></div><div class="descriptionBox"><h3>${data.name}</h3><p>${data.description}</p></div></div>`
+const dataCb = (x) => (beerSectionNode.innerHTML += printBeer(x));
+const printBeer = (data) =>
+  `<div class="tarjetClass"><div class="imgContainer"><img src="${data.image_url}"></div><div class="descriptionBox"><a href="../description/description.html?ID=${data.id}"><h3>${data.name}</h3></a></div></div>`;
 
 const fetchedData = (url) => {
-    return fetch(url)
-    .then(res => res.json())
-} 
-    
-fetchedData(apiUrl)
-    .then(data => {
-        data.map(dataCb)
-        return data
-        
-})
+  return fetch(url).then((res) => res.json());
+};
 
+let beersArr = [];
+
+fetchedData(apiUrl).then((data) => {
+  data.map(dataCb);
+  return data;
+});
+
+const allFetchedBeer = (urlArr) => {
+  urlArr.map((url) => {
+    return fetchedData(url).then((data) => {
+      return data.map((element) => beersArr.push(element.name));
+    });
+  });
+};
+
+allFetchedBeer(allBeerArr);
 
 myPlusBnt.addEventListener("click", (e) => {
-    if (counterObj.counter >= 13) {
-        return e.preventDefault()
-    } else {
-        counterObj.counter++
-        beerSectionNode.innerHTML = ""
-        fetchedData(`https://api.punkapi.com/v2/beers?page=${counterObj.counter}&per_page=25`)
-            .then(data => data.map(dataCb))
-    }
-    
-    
-})
+  if (counterObj.counter >= 32) {
+    return e.preventDefault();
+  } else {
+    counterObj.counter++;
+    beerSectionNode.innerHTML = "";
+    fetchedData(
+      `https://api.punkapi.com/v2/beers?page=${counterObj.counter}&per_page=10`
+    ).then((data) => data.map(dataCb));
+  }
+});
 
 myLessBnt.addEventListener("click", (e) => {
-    if (counterObj.counter <= 1) {
-        return e.preventDefault() 
-    } else {
-        counterObj.counter--
-        beerSectionNode.innerHTML = ""
-        fetchedData(`https://api.punkapi.com/v2/beers?page=${counterObj.counter}&per_page=25`)
-            .then(data => data.map(dataCb))     
-    }        
-})
+  if (counterObj.counter <= 1) {
+    return e.preventDefault();
+  } else {
+    counterObj.counter--;
+    beerSectionNode.innerHTML = "";
+    fetchedData(
+      `https://api.punkapi.com/v2/beers?page=${counterObj.counter}&per_page=10`
+    ).then((data) => data.map(dataCb));
+  }
+});
+let beerSearch = "";
+let timer;
 
+function debounce(search, timeout = 300, node) {
+  node.innerHTML = "Loading";
+  return new Promise((res, rej) => {
+    clearTimeout(timer);
+    if (search == "") return;
+    timer = setTimeout(async () => {
+      const awaitedData = await fetchedData(
+        `https://api.punkapi.com/v2/beers?beer_name=${search}`
+      );
+      res(awaitedData);
+    }, timeout);
+  });
+}
 
+searchNode.addEventListener("keyup", (e) => {
+  beerSearch = e.target.value;
+  if (beerSearch === "") {
+    beerSectionNode.innerHTML = "";
+    fetchedData(apiUrl).then((data) => {
+      data.map(dataCb);
+    });
+  }
+  debounce(beerSearch, 1000, beerSectionNode).then((data) => {
+    beerSectionNode.innerHTML = "";
+    data.map(dataCb);
+  });
 
+  // beerSectionNode.innerHTML = ""
+  // let beersFilter = beersArr.filter((element) => {
+  //     return element.toLowerCase().includes(beerSearch)
 
+  // })
+  // console.log(beersFilter)
+});
 
-
-
+randomBeer.addEventListener("click", () => {
+  location.href = "../description/description.html?ID=random";
+});
--- description/description.js
+++ description/description.js
@@ -3,19 +3,23 @@
 const beerSectionNode = document.getElementById("randomWrapper")
 const randomSelector = document.getElementById("randomBtn")
 const randomApiUrl = "https://api.punkapi.com/v2/beers/random"
-
-
-
+const url = "https://api.punkapi.com/v2/beers/"
 
 const dataCb = x => beerSectionNode.innerHTML += printBeer(x)
 const printBeer = data => `<div class="tarjetClass"><div class="imgContainer"><img src="${data.image_url}"></div><div class="descriptionBox"><h3>${data.name}</h3><p>${data.description}</p></div></div>`
+
+
+const urlParams = new URLSearchParams(window.location.search);
+let urlID = urlParams.get("ID");
+
 
 const fetchedData = (url) => {
     return fetch(url)
     .then(res => res.json())
 } 
-    
-fetchedData(randomApiUrl)
+
+
+fetchedData(url + urlID)
     .then(data => {
         data.map(dataCb)
         return data
