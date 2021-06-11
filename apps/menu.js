cb0860ded00b121de139fb5c73518304b4298752
Diego González <diegoztrombone@gmail.com>
2021-06-11 17:55:04 +0200

    feat: barra de navegación con animaciones, en toda la web, portada, y eleccion de colores

--- apps/menu.js
+++ apps/menu.js
@@ -1,7 +1,9 @@
 const menuNode = document.getElementById("menu");
 const navNode = document.getElementById("nav");
+const bgNode = document.getElementById("menu-bg");
 
 menuNode.addEventListener("click", () => {
   menuNode.classList.toggle("change");
   navNode.classList.toggle("change");
+  bgNode.classList.toggle("change-bg");
 });
--- description/description.js
+++ description/description.js
@@ -1,13 +1,13 @@
 // RANDOM JS
 
-const beerSectionNode = document.getElementById("randomWrapper");
-const randomSelector = document.getElementById("randomBtn");
+const beerSectionNode = document.getElementById("descriptionWrapper");
+const beerDescriptionNode = document.getElementById("randomBtn");
 const randomApiUrl = "https://api.punkapi.com/v2/beers/random";
 const url = "https://api.punkapi.com/v2/beers/";
 
 const dataCb = (x) => (beerSectionNode.innerHTML += printBeer(x));
 const printBeer = (data) =>
-  `<div class="beerDescriptionContainer"><div class="descriptionImg"><img src="${data.image_url}"></div><div class="descriptionBeer"><h3>${data.name}</h3><p>${data.description}</p></div></div>`;
+  `<div class="beerContainer"><div class="descriptionImg"><img src="${data.image_url}"></div><div class="descriptionText"><h3>${data.name}</h3><p>${data.description}</p></div></div>`;
 
 const urlParams = new URLSearchParams(window.location.search);
 let urlID = urlParams.get("ID");
@@ -21,7 +21,7 @@ fetchedData(url + urlID).then((data) => {
   return data;
 });
 
-randomSelector.addEventListener("click", () => {
+beerDescriptionNode.addEventListener("click", () => {
   beerSectionNode.innerHTML = "";
   fetchedData(randomApiUrl).then((data) => {
     data.map(dataCb);
